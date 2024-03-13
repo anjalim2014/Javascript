70e663c057a7f3771a827f5874cbdc1407e1b0d3
Anjali Murali <73739330+anjalim2014@users.noreply.github.com>
2024-03-13 11:39:29 +0000

    Update script.js

--- Calc/script.js
+++ Calc/script.js
@@ -1,4 +1,4 @@
-console.log('ssss')
+
 const clicked=(value)=>{
     document.querySelector('.input-all').innerHTML+=value
     document.getElementById('input').innerHTML+=value
@@ -12,12 +12,6 @@ const clearDisplay=()=>{
     document.getElementById('input').innerHTML=null
     document.querySelector('.input-all').innerHTML=null
 }
-// const operation=(value)=>{
-//     let input_1=document.getElementById('input').innerText
-//     document.getElementById('input').value=0;
-    
-//     console.log(document.getElementById('input').value)
-// } 
 const equalClick=()=>{
     let exp=document.querySelector(".input-all").innerText
     result=eval(exp)
